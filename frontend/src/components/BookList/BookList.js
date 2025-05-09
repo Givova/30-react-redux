6bca132f8618b8f027a43d66f968dcd6a46d004b
Kukushka <gusarov.ml@mail.ru>
2025-05-09 15:38:44 +0300

    Add function "Delete"

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,8 +1,16 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteBook } from "../../redux/books/actionCreators";
 import "./BookList.css";
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleDeleteBook = (id) => {
+    //deletebook
+    console.log(deleteBook(id));
+    dispatch(deleteBook(id));
+  };
 
   return (
     <div className="app-block book-list">
@@ -15,6 +23,11 @@ const BookList = () => {
             <li key={book.id}>
               <div className="book-info">
                 {++i}. {book.title} by <strong>{book.author}</strong>
+              </div>
+              <div className="book-action">
+                <button onClick={() => handleDeleteBook(book.id)}>
+                  Delete
+                </button>
               </div>
             </li>
           ))}
