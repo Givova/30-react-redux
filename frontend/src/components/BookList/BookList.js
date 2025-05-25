4b7e438d6a840a6dfe5af9d625bd708eab8d9da0
Kukushka <gusarov.ml@mail.ru>
2025-05-25 21:42:35 +0300

    Created booksSlice instatead of regular reduce

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
-import { deleteBook, toggleFavorite } from "../../redux/books/actionCreators";
+// import { deleteBook, toggleFavorite } from "../../redux/books/actionCreators";
+import { deleteBook, toggleFavorite, selectBooks } from "../../redux/slices/booksSlice";
 import { BsBookmarkStarFill, BsBookmarkStar } from "react-icons/bs";
 import { selectTitleFilter, selectAuthorFilter, selectOnlyFavoriteFilter } from "../../redux/slices/filterSlice";
 import "./BookList.css";
 
 const BookList = () => {
-  const books = useSelector((state) => state.books);
+  const books = useSelector(selectBooks);
   const titleFilter = useSelector(selectTitleFilter);
   const authorFilter = useSelector(selectAuthorFilter);
   const onlyFavoriteFilter = useSelector(selectOnlyFavoriteFilter);
