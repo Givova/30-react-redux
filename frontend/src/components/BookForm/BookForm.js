149d0752318378cafd729f14add23f865377e14a
Kukushka <gusarov.ml@mail.ru>
2025-07-06 20:19:07 +0300

    Finaly version. edit initial state. load in redux state.

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { FaSpinner } from "react-icons/fa";
 import { setError } from "../../redux/slices/errorSlice";
-import { addBook, fetchBook } from "../../redux/slices/booksSlice";
+import { addBook, fetchBook, selectIsLoadingViaAPI } from "../../redux/slices/booksSlice";
 import createBookWithID from "../../utils/createBookWithID";
 import booksData from "../../data/books.json";
 import "./BookForm.css";
@@ -9,6 +10,8 @@ import "./BookForm.css";
 const BookForm = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
+  // const [isLoading, setIsLoading] = useState(false);
+  const isLoadingViaAPI = useSelector(selectIsLoadingViaAPI);
   const dispatch = useDispatch();
 
   const handleAddRandomBook = () => {
@@ -31,8 +34,16 @@ const BookForm = () => {
   };
 
   const handleAddRandomBookViaAPI  = () => {
-      dispatch(fetchBook('http://localhost:4000/random-book'))
- 
+
+    dispatch(fetchBook('http://localhost:4000/random-book-delayed'))
+
+    // try {
+    //   // setIsLoading(true)
+    //   await dispatch(fetchBook('http://localhost:4000/random-book-delayed'))
+    // } finally {
+    //   // setIsLoading(false) // Нет необходимости, так как уже обрабатывает бек
+    // }
+  
   }
   return (
     <div className="app-block book-form">
@@ -60,8 +71,19 @@ const BookForm = () => {
         <button type="button" onClick={handleAddRandomBook}>
           Add Random
         </button>
-    <button type="button" onClick={handleAddRandomBookViaAPI}>
-        Add random book via API
+       
+    <button 
+        type="button" 
+        onClick={handleAddRandomBookViaAPI} 
+        disabled={isLoadingViaAPI}
+    >
+      {isLoadingViaAPI ? (
+        <>
+        <span>Loading Book...</span>
+        <FaSpinner className="spinner" />
+        </>
+      ) : "Add random book via API"
+    }  
     </button>
 
       </form>
--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -3,7 +3,11 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import createBookWithID from "../../utils/createBookWithID";
 import { setError } from "./errorSlice";
 
-const initialState = [];
+const initialState = {
+  books: [],
+  isLoadingViaAPI: false,
+
+};
 
 export const fetchBook = createAsyncThunk(
   'books/fetchBook',
@@ -13,7 +17,8 @@ export const fetchBook = createAsyncThunk(
       return res.data
    } catch (error) {
       thunkAPI.dispatch(setError(error.message))
-      throw error
+      // throw error  // можно создавать и так. Создаем ошибку, чтобы промис уходил в rejected, а не в fullfield.
+      return thunkAPI.rejectWithValue(error.message) // можно так. Создаем ошибку, чтобы промис уходил в rejected, а не в fullfield.
    }
     
   }
@@ -24,35 +29,38 @@ const booksSlice = createSlice({
   initialState,
   reducers:{
     addBook: (state, action) => {
-        // state.push(action.payload); // Благодаря Immer, можно мутировать состояние напрямую
-        return [...state, action.payload]; // Альтернативный иммутабельный способ
+        state.books.push(action.payload); // Благодаря Immer, можно мутировать состояние напрямую
+        // return [...state.books, action.payload]; // Альтернативный иммутабельный способ
     },
     deleteBook: (state, action) => {
-      return state.filter((book) => book.id !== action.payload);
+      return {...state, books: state.books.filter((book) => book.id !== action.payload)}
     },
     toggleFavorite: (state, action) => {
-        state.forEach((book) => {
+        state.books.forEach((book) => {
             if(book.id === action.payload){
                 book.isFavorite = !book.isFavorite;
             }
-        })
+        });
         
       
-    }
+    },
 
   },
   extraReducers: (builder) => {
+    builder.addCase(fetchBook.pending, (state, action) => {
+      state.isLoadingViaAPI = true;
+    });
     builder.addCase(fetchBook.fulfilled, (state, action) => {
-     
+      state.isLoadingViaAPI = false;
       if(action.payload?.title && action.payload?.author){
-        state.push(createBookWithID(action.payload, 'API'))
+        state.books.push(createBookWithID(action.payload, 'API'))
       } 
     });
-   
-   
-  }
-  
-})
+    builder.addCase(fetchBook.rejected, (state, action) => {
+      state.isLoadingViaAPI = false;
+    });
+  },
+});
 
 
 
@@ -69,5 +77,6 @@ export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
     
 //   }
 
-export const selectBooks = (state) => state.books;
+export const selectBooks = (state) => state.books.books;
+export const selectIsLoadingViaAPI = (state) => state.books.isLoadingViaAPI;
 export default booksSlice.reducer;
